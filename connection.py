a95820cba88fe3cd0f815800feaca3f96d1dbdcc
Tauane Lima <65177647+TauaneLima@users.noreply.github.com>
2022-04-19 22:06:26 -0300

    Update connection.py

--- connection.py
+++ connection.py
@@ -1,20 +1,27 @@
-from flask import Flask, render_template, json, request
+from flask import Flask, render_template, request
 from flaskext.mysql import MySQL
 
 mysql = MySQL()
 app = Flask(__name__)
+app.url_map.strict_slashes = False
 
-    # Criar Conexão com Banco SQLILITE
+# MySQL configurations
 app.config['MYSQL_DATABASE_USER'] = 'root'
 app.config['MYSQL_DATABASE_PASSWORD'] = 'admin'
 app.config['MYSQL_DATABASE_DB'] = 'teste'
 app.config['MYSQL_DATABASE_HOST'] = '172.17.0.2'
-
+#app.config['MYSQL_DATABASE_HOST'] = ''
 mysql.init_app(app)
+
+
+conexao = mysql.connect()
+cursor = conexao.cursor()
+
 
 @app.route('/')
 def main():
     return render_template('home.html')
+
 
 @app.route('/login.html')
 def login():
@@ -28,41 +35,33 @@ def leitores():
 def leitores():
     return render_template('books.html')
 
-@app.route("/fazerlogin", methods=['POST', 'GET'])
-def fazerlogin():
-    try:
-        _name = request.form['inputName']
-        _email = request.form['inputEmail']
-        _password = request.form['inputPassword']
+@app.route("/adicionar_usuario", methods=['POST', 'GET'])
+def incluir_usuario():
+    cursor = conexao.cursor()
+    nome = request.form['nome']
+    email = request.form['email']
+    senha = request.form['senha']
+    cursor.execute("""INSERT INTO tbl_user(user_name, user_username, user_password) VALUES(%s, %s, %s)""", (nome, email, senha))
+    conexao.commit()
+    cursor.close()
+    return render_template('login.html')
 
-        print(_name)
-        print(_email)
-        print(_password)
 
-        # validate the received values
-        if _name and _email and _password:
-            
-            conn = mysql.connect()
-            cursor = conn.cursor()
-            _hashed_password = _password
-            cursor.callproc('sp_createUser',(_name,_email,_hashed_password))
-            data = cursor.fetchall()
-            conn.commit()
-            print(data);
-        return render_template('login.html')
-  
-@app.route('/list',methods=['POST','GET'])
-def list():
-    try:
-            conn = mysql.connect()
-            cursor = conn.cursor()
-            cursor.execute ('select user_name, user_username from tbl_user')
-            data = cursor.fetchall()
-            print(data[0]);
+@app.route("/listar_usuario", methods=['POST', 'GET'])
+def listar_usuario():
+    consulta_bd = "select * from tb_user"
+    cursor = conexao.cursor()
+    cursor.execute(consulta_bd)
+    details = cursor.fetchall()
+    print("Quantidade de registros: ", cursor.rowcount)
 
-            conn.commit()
-            return render_template('leitores.html', datas=data)
-    
+    for x in range(len(details)):
+        print(details[x])
+    conexao.commit()
+    cursor.close()
+    conexao.close()
+    return render_template('usuarios.html')
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5050, debug=True)
